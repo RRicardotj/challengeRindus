526b69ce86a7d140d8b13c033f898f78475ad88b
Ricardo Rodriguez (rricardotj) <tjrprecursor@gmail.com>
2022-03-23 16:59:27 +0100

    Create header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <div class="header__logo">Ricardo's Posts</div>
+    <div class="header__user-info"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 56px;
+  padding: 0 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: sticky;
+}
+</style>
