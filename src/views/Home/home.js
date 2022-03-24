36a7fb407008ef4d1e84e93b97dde1d71e34d052
Ricardo Rodriguez (rricardotj) <tjrprecursor@gmail.com>
2022-03-24 13:33:07 +0100

    feat: Create base logic of home view

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -1,8 +1,35 @@
 import SimpleLayout from '@/components/SimpleLayout.vue';
+import { getAll } from '@/services/postService';
 
 export default {
   name: 'HomeView',
   components: {
     SimpleLayout,
+  },
+  data: () => ({
+    posts: [],
+  }),
+  methods: {
+    async getAllPosts() {
+      try {
+        const { data } = await getAll();
+
+        console.log(data);
+
+        this.posts = data;
+      } catch (e) {
+        // this should be handler with an error handler implementing some toast to show notifications
+        this.posts = null;
+      }
+    },
+  },
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mount', this.posts);
+    if (this.posts && !this.posts.length) {
+      this.getAllPosts();
+    }
   },
 };
