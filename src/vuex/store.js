230846103592c3e1e84cdf9c8a33f1edc99a7ba3
Ricardo Rodriguez (rricardotj) <tjrprecursor@gmail.com>
2022-03-28 08:00:42 +0200

    add global states to store

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -5,6 +5,12 @@ import PostsMutations from './posts/mutations';
 import UsersState from './users/state';
 import UsersActions from './users/actions';
 import UsersMutations from './users/mutations';
+import PostFoundState from './post/state';
+import PostFoundActions from './post/actions';
+import PostFoundMutations from './post/mutations';
+import CommentsState from './comments/state';
+import CommentsActions from './comments/actions';
+import CommentsMutations from './comments/mutations';
 
 export const state = {
   loggedUser: {
@@ -13,16 +19,22 @@ export const state = {
   },
   ...PostsState,
   ...UsersState,
+  ...PostFoundState,
+  ...CommentsState,
 };
 
 export const mutations = {
   ...PostsMutations,
   ...UsersMutations,
+  ...PostFoundMutations,
+  ...CommentsMutations,
 };
 
 export const actions = {
   ...PostsActions,
   ...UsersActions,
+  ...PostFoundActions,
+  ...CommentsActions,
 };
 
 export const getters = {
